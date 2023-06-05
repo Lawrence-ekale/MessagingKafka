8117dc8622b9539965417b8c6c9ee467e52b3230
Lawrence-ekale <lawrenceekale2@gmail.com>
2023-06-05 11:19:19 +0300

    Messaging microservice using kafka

---
+++ src/test/java/com/lawrenceekale/MessagingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrenceekale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
